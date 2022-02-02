6040ea0ff986b24dee57a5dd3e40833eee1f99a5
mt194 <moetraboulsi194@gmail.com>
2022-02-02 17:33:06 +0200

    Added bodyparser

--- src/backend/server_connect.js
+++ src/backend/server_connect.js
@@ -1,12 +1,21 @@
+//modules
 const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//file modules 
+//const userRoutes = require('./routes/user_routes'); ****Example (uncomment later)****
 
 //configuring the enviroment variable for the mongo URI string
 dotenv.config();
 
 //creating the express app
 const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));   //can now access url encoded form request bodies with req.body
 
 //port to be used for requests
 const PORT = process.env.PORT;
@@ -18,3 +27,6 @@ const mongoURI = process.env.MONGO_CONNECTION_URL;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)))
   .catch((err) => console.log(err));
+
+
+
