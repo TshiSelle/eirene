122a4386515d4635e4e56b080799bda58de21ee5
mt194 <moetraboulsi194@gmail.com>
2022-02-04 10:16:22 +0200

    Joining account functions under account/ path

--- src/backend/controllers/accountController.js
+++ src/backend/controllers/accountController.js
@@ -1,23 +1,57 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-const router = express.Router();
-
 
 dotenv.config();
-//Expected login credential as request (username and password)
-router.post('/', (req, res) => {
-    const userLogginIn = req.body;
+
+// Expected Request : Receive a JSON, filled with user data
+const register = async (req, res) => {
+    const user = req.body;
+
+    //check if user's email/username are already taken
+    const takenUsername = await User.findOne({ username: user.username.toLowerCase() });
+    const takenEmail = await User.findOne({ email: user.email });
+
+    if (takenUsername) {
+        res.status(400).json({ message: 'Username is already in use' });
+    }
+    else if (takenEmail) {
+        res.status(400).json({ message: 'Email is already in use' });
+    }
+    else {
+        //hash the password before storing in DB (salt should be auto-generated)
+        user.password = await bcrypt.hash(req.body.password, 10);
+
+        const dbUser = new User({
+            fname: user.fname,
+            lname: user.lname,
+            username: user.username.toLowerCase(),
+            email: user.email.toLowerCase(),
+            password: user.password 
+        });
+
+        dbUser.save()
+            .then(() => {
+                res.status(201).json({ message: 'Succesfully created user account' });
+            })
+            .catch((err) => {
+                console.log(`Error occurred while storing user account in database : ${err}`);
+            })
+    }
+}
+
+//Expected Request : Receive login credentials (username and password)
+const login = (req, res) => {
+    const userLoggingIn = req.body;
 
     //find the received username in database
-    User.findOne({ username: userLogginIn.username.toLowerCase() })
+    User.findOne({ username: userLoggingIn.username.toLowerCase() })
         .then((dbUser) => {
             if (!dbUser) {
                 return res.status(401).json({ messsage: 'Invalid username or password' });
             }
-            bcrypt.compare(userLogginIn.password,dbUser.password) //verify password
+            bcrypt.compare(userLoggingIn.password,dbUser.password) //verify password
                 .then((validPassword) => {
                     if (validPassword) { //valid password, sends an authentication token
                         // Don't put sensitive data here, only data you want the client to have such as name (to view in profile page)
@@ -48,6 +82,9 @@ router.post('/', (req, res) => {
                     }
                 })
         })
-});
+}
 
-module.exports = router;
+module.exports = {
+    register,
+    login
+};
