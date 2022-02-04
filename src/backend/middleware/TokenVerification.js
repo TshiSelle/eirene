8c9a9e20f2431a0c10e1505115b3e53710eb3500
mt194 <moetraboulsi194@gmail.com>
2022-02-04 14:01:27 +0200

    Code format changes

--- src/backend/middleware/TokenVerification.js
+++ src/backend/middleware/TokenVerification.js
@@ -5,15 +5,12 @@ dotenv.config();
 
 //passing in the verifyJWT middleware as the second parameter to app.get() lets us access the current user's data 
 function verifyJWT (req, res, next) {
-    const token = req.headers['x-acccess-token']?.split(' ')[1];   //extract token from header (without the 'Bearer' tag)
+    const token = req.headers['x-access-token']?.split(' ')[1];   //extract token from header (without the 'Bearer' tag)
 
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) {                                             // error during jwt verificaiton
-                return res.status(401).json({
-                    isLoggedIn: false,
-                    message: 'Authentication failed'
-                });
+            if (err) {                                             // error during jwt verification
+                return res.status(401).json({ isLoggedIn: false, message: 'Authentication failed' });
             }
             else {
                 req.user = {};
