3104e1e4ad7854e4fe1222917155c7902ae8d976
OmarHoussami <omar.houssami20@gmail.com>
2022-02-04 17:22:13 +0200

    Formatting updates

--- src/backend/middleware/TokenVerification.js
+++ src/backend/middleware/TokenVerification.js
@@ -4,19 +4,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 //passing in the verifyJWT middleware as the second parameter to app.get() lets us access the current user's data 
-function verifyJWT (req, res, next) {
+function verifyJWT(req, res, next) {
     const token = req.headers['x-access-token']?.split(' ')[1];   //extract token from header (without the 'Bearer' tag)
 
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) {                                             // error during jwt verification
+            if (err) {  // error during jwt verification
                 return res.status(401).json({ isLoggedIn: false, message: 'Authentication failed' });
             }
             else {
                 req.user = {};
                 req.user.id = decoded.id;
                 req.user.username = decoded.username
-                next();                   // if verification succeeds we can advance to the route and send back isLoggedIn set to true
+                next();// if verification succeeds we can advance to the route and send back isLoggedIn set to true
             }
         });
     }
