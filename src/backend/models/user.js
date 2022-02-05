b8175a46a018737c3f3bcf21aa818a5d805cc625
mt194 <moetraboulsi194@gmail.com>
2022-02-05 15:14:49 +0200

    Add gender field

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -2,32 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  fname: {
-    type: String,
-    required: true
-  },
-  lname: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  _journalid: {
-    type: Schema.Types.ObjectId,
-    ref: 'Journal'
-  }
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  gender: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verified: { type: Boolean, default: false },
+  uniqueString: { type: String },
+  _journalid: { type: Schema.Types.ObjectId, ref: 'Journal' }
 
 },{ timestamps: true })
 
