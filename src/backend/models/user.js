48178b5963fa2fb95b83adccc6d0f2e49a0d6a06
mt194 <moetraboulsi194@gmail.com>
2022-02-05 13:13:36 +0200

    Added double checking duplicate emails/usernames

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -12,11 +12,13 @@ const userSchema = new Schema({
   },
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
