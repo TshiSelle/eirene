9211c2ee0749274597f383f7ddcd2c2286112cbb
mt194 <moetraboulsi194@gmail.com>
2022-02-06 11:15:41 +0200

    Tidying up code

--- src/backend/controllers/accountController.js
+++ src/backend/controllers/accountController.js
@@ -2,26 +2,22 @@ const { User } = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-const Validator = require('validator');
-const isEmpty = require('is-empty');
 const crypto = require('crypto');
-const nodemailer = require('nodemailer');
+const sendEmail = require('../helperFunctions/emailSender');
+const { validateLoginInput, validateRegisterInput } = require('../helperFunctions/inputValidation');
 dotenv.config();
 
 // Expected Request : Receive a JSON, filled with user data
 const register = async (req, res) => {
     const user = req.body;
-
-    
     const { errors, isValid } = validateRegisterInput(user);
-
     if (!isValid) {
         res.status(400).json(errors);
     } 
     else {
         //check if user's email/username are already taken
         const takenUsername = await User.findOne({ username: user.username.toLowerCase() });
-        const takenEmail = await User.findOne({ email: user.email });
+        const takenEmail = await User.findOne({ email: user.email.toLowerCase() });
         if (takenUsername) {
             res.status(400).json({ message: 'Username is already in use' });
         } else if (takenEmail) {
@@ -110,104 +106,6 @@ const verifyEmail = async (req, res) => {
     }
 };
 
-//********** Input validation functions **********
-
-function validateRegisterInput(data) {
-    let errors = {};
-
-    //converting empty fields to strings
-    data.fname = !isEmpty(data.fname) ? data.fname : '';
-    data.lname = !isEmpty(data.lname) ? data.lname : '';
-    data.gender = !isEmpty(data.gender) ? data.gender : '';
-    data.email = !isEmpty(data.email) ? data.email : '';
-    data.username = !isEmpty(data.username) ? data.username : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
-    data.confirmPassword = !isEmpty(data.confirmPassword) ? data.confirmPassword : '';
-    
-    //field checks (empty input / password rules / etc..)
-    //Name Checks
-    if (Validator.isEmpty(data.fname)) {
-        errors.fname = 'First name field is required';
-    } 
-    if (Validator.isEmpty(data.lname)) {
-        errors.lname = 'Last name field is required';
-    }
-    //Gender Check
-    if (Validator.isEmpty(data.gender)) {
-        errors.gender = 'Gender field is required';
-    } else if (!(Validator.equals(data.gender,'male') || Validator.equals(data.gender,'female'))) {
-        errors.gender = 'Wrong input in gender field'
-    }
-    //Email Check
-    if (Validator.isEmpty(data.email)) {
-        errors.email = 'Email field is required';
-    } else if (!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
-    }
-    //Username Check
-    if (Validator.isEmpty(data.username)) {
-        errors.username = 'Username field is required';
-    }
-    //Password check
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password field is required';
-    } else if (!Validator.isLength(data.password, { min: 8, max: 64 })) {
-        errors.password = `Password must be at least 8 characters long${data.password.length > 64 ? ' and less than 64': ''}`;
-    } else if (Validator.isEmpty(data.confirmPassword)) {
-        errors.confirmPassword = 'Confirm Password field is required';
-    } else if (!Validator.equals(data.password,data.confirmPassword)) {
-        errors.confirmPassword = 'Passwords must match';
-    }
-    return { errors, isValid: isEmpty(errors) };
-    
-}
-
-function validateLoginInput(data) {
-    const errors = {};
-    //converting empty fields to strings
-    data.username = !isEmpty(data.username) ? data.username : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
-    
-    if (Validator.isEmpty(data.username)) {
-        errors.username = 'Username field is required';
-    }
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password field is required';
-    }
-    return { errors, isValid: isEmpty(errors) };
-}
-
-
-//*************** Email transfer function ******************
-
-function sendEmail(email, uniqueString) {
-    var Transport = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'loom.senior@gmail.com',
-            pass: process.env.LOOM_EMAIL_PASSWORD
-        }
-    });
-
-    let sender = 'Eirene';
-    var mailOptions = {
-        from: sender,
-        to: email,
-        subject: 'Eirene account verification',
-        html: `hi kifak, press <a href=http://localhost:${process.env.PORT}/account/verify/${uniqueString}>here</a> to verify your account!`
-    };
-
-    Transport.sendMail(mailOptions, (error, response) => {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            console.log('Email sent')
-        }
-    });
-
-
-}
 
 module.exports = {
     register,
