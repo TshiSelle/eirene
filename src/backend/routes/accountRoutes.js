69d1916b69bc2c91a545e33730da4bc2fee6d80a
mt194 <moetraboulsi194@gmail.com>
2022-02-11 22:55:34 +0200

    Small route change

--- src/backend/routes/accountRoutes.js
+++ src/backend/routes/accountRoutes.js
@@ -6,12 +6,12 @@ const verifyJWT = require('../middleware/TokenVerification');
 
 
 
-router.post('/login', accounts.login);
-router.get('/verify/:emailVerificationToken', accounts.verifyEmail)
-router.post('/changePass', verifyJWT, accounts.changePassword)
+router.post('/login', accounts.login);;
+router.get('/verify/:emailVerificationToken', accounts.verifyEmail);
+router.post('/changePass', verifyJWT, accounts.changePassword);
 router.get('/forgotPass',accounts.forgotPassword);
-router.post('/resetPass/submit/:username/:passResetToken', accounts.resetPass)
-router.get('/resetPass/:username/:passResetToken', accounts.resetPassPage)
+router.post('/resetPass/:username/:passResetToken', accounts.resetPass)
+    .get('/resetPass/:username/:passResetToken', accounts.resetPassPage);
 
 
 module.exports = router;
