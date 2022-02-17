4c243a1ac9654bed040880b417c17e16ae0565df
mt194 <moetraboulsi194@gmail.com>
2022-02-17 11:27:32 +0200

    Add therapist schema

---
+++ src/backend/models/therapist.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const therapistSchema = new Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  gender: { type: String, required: true },
+  degree: { type: String },
+  university: { type: String },
+  officeAddress: { type: String },
+  phone: { type: String },
+  description: { type: String },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+}, { timestamps: true })
+
+const Therapist = mongoose.model('Therapist', therapistSchema);
+
+module.exports = Therapist;
