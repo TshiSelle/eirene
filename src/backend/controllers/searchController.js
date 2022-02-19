9e6c2ec27c7b5bdb68d4f378d6b2fcc6464e5ced
mt194 <moetraboulsi194@gmail.com>
2022-02-19 17:39:58 +0200

    Added more search filters

--- src/backend/controllers/searchController.js
+++ src/backend/controllers/searchController.js
@@ -1,21 +1,29 @@
 const isEmpty = require('is-empty');
 const { Therapist } = require('../models/therapist');
+const { getSearchConfig } = require('../helperFunctions/queryExtraction');
 
 const searchTherapists = async (req, res) => {
     if (isEmpty(req.query.searchString)) {
         res.status(400).json({ message: 'Search field is empty, please add input', success: false });
     }
+   
     try {
         const searchResults = await Therapist.find({ 
             $or: [ { fname : { $regex: ".*"+req.query.searchString+".*", $options: "i" } },
                    { lname : { $regex: ".*"+req.query.searchString+".*", $options: "i" } },
                    { $text : { $search: req.query.searchString } }
-                ],
-            
+            ],
+            $and: [
+                { gender : req.query.gender || { $regex: /.*/ } },
+                { degree : req.query.degree || { $regex: /.*/ } },
+                { yearsOfExperience : req.query.yoe || { $gte: 0 } },
+                { title : { $regex: req.query.title ?  ".*"+req.query.title+".*" : /.*/, $options: "i" } },
+            ],
          });
         
-         res.status(200).json({ searchResults, success: true })
+         res.status(200).json({ numOfResults: searchResults.length, success: true, searchResults })
     } catch (error) {
+        console.log(error)
         return res.status(400).json({ message: `Error occurred while searching for therapists`, success: false, error: error })
     }
     
@@ -24,3 +32,4 @@ const searchTherapists = async (req, res) => {
 };
 
 module.exports = { searchTherapists };
+
