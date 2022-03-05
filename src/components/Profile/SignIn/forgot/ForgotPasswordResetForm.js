eca5436b0de39bef3b297c612d024bcd5692c70c
lynn <lynn.serhan2001@gmail.com>
2022-03-05 17:16:53 +0200

    reset password page

--- src/components/Profile/SignIn/forgot/ForgotPasswordResetForm.js
+++ src/components/Profile/SignIn/forgot/ForgotPasswordResetForm.js
@@ -1,9 +1,17 @@
 import React, { useReducer, useCallback, useEffect } from "react";
 import styled from "styled-components";
 import { Form, Alert, Button } from "react-bootstrap";
-import { validatePassword, validateConfirmPassword } from '../../../../validators/validators';
+import {
+  validatePassword,
+  validateConfirmPassword,
+} from "../../../../validators/validators";
 import { useParams } from "react-router-dom";
-import { PasswordResetWithToken, IsEmailTokenValid } from '../../../../api/ApiClient';
+import {
+  PasswordResetWithToken,
+  IsEmailTokenValid,
+} from "../../../../api/ApiClient";
+import "./forgotPassStyle.css";
+import background from "./bg_4.jpg";
 
 const reducer = (state, action) => {
   // These cases are taken into consideration by the dispatches used in the useCallbacks down below,
@@ -28,9 +36,9 @@ const reducer = (state, action) => {
         submissionErrorMessage: null,
       };
     case "token-success":
-        return { ...state, emailValid: true };
+      return { ...state, emailValid: true };
     case "token-failure":
-        return { ...state, emailValid: false };
+      return { ...state, emailValid: false };
     case "forgot-password-start":
       return { ...state, waiting: true };
     case "forgot-password-success":
@@ -47,7 +55,7 @@ const reducer = (state, action) => {
 };
 
 const ForgotPasswordResetForm = () => {
-    const { username, token } = useParams();
+  const { username, token } = useParams();
   const [state, dispatch] = useReducer(reducer, {
     password: "",
     confirmPassword: "",
@@ -65,23 +73,23 @@ const ForgotPasswordResetForm = () => {
     submissionErrorMessage,
     finished,
     waiting,
-    emailValid
+    emailValid,
   } = state;
 
   useEffect(() => {
     IsEmailTokenValid(username, token)
-    .then((response) => {
-      if (response.data.success) {
-        dispatch({ type: "token-success" });
-        console.log('Successful forgotpass!');
-      } else {
+      .then((response) => {
+        if (response.data.success) {
+          dispatch({ type: "token-success" });
+          console.log("Successful forgotpass!");
+        } else {
+          dispatch({ type: "token-failure" });
+        }
+      })
+      .catch(() => {
         dispatch({ type: "token-failure" });
-      }
-    })
-    .catch(() => {
-      dispatch({ type: "token-failure" });
-      return;
-    });
+        return;
+      });
   }, [username, token]);
 
   const setPassword = useCallback(
@@ -119,12 +127,15 @@ const ForgotPasswordResetForm = () => {
     }
     dispatch({ type: "forgot-password-start" });
     PasswordResetWithToken(username, token, password, confirmPassword)
-    .then((response) => {
+      .then((response) => {
         if (response.data.success) {
           dispatch({ type: "forgot-password-success" });
-          console.log('Successful qwerqwrqwerqerq!');
+          console.log("Successful qwerqwrqwerqerq!");
         } else {
-          dispatch({ type: "forgot-password-failure", errorMessage: response.data.message });
+          dispatch({
+            type: "forgot-password-failure",
+            errorMessage: response.data.message,
+          });
         }
       })
       .catch((error) => {
@@ -137,52 +148,121 @@ const ForgotPasswordResetForm = () => {
   }, [waiting, finished, password, confirmPassword]);
 
   return (
-      <>
-    {emailValid
-    ?
-    <Form className="signup-form" onSubmit={resetUserPassword}>
-        <GridContainer>
-        <Label>Password</Label>
-        <Label>Confirm Password</Label>
+    <>
+      {emailValid ? (
+        <MainContainer>
+          <MainSection>
+            <FormContainer>
+              <ResponsiveContainer>
+                <Header>Reset Password</Header>
+                <Subheader>
+                  Choose your new password and traverse Eirene again.
+                </Subheader>
 
-        <Form.Control
-            className="textField"
-            type="password"
-            isInvalid={passwordError}
-            placeholder=""
-            name="password"
-            value={password}
-            onChange={setPassword}
-        />
+                <Form className="signup-form" onSubmit={resetUserPassword}>
+                  <Label>Password</Label>
+                  <Form.Control
+                    className="textField"
+                    type="password"
+                    isInvalid={passwordError}
+                    placeholder=""
+                    name="password"
+                    value={password}
+                    onChange={setPassword}
+                  />
 
-        <Form.Control
-            className="textField"
-            type="password"
-            isInvalid={passwordError}
-            placeholder=""
-            name="confirmPassword"
-            value={confirmPassword}
-            onChange={setConfirmPassword}
-        />
-        </GridContainer>
-          <Button
-          value="Submit"
-          type="submit"
-          style={{ height: 50, width: 100}}
-          disabled={submissionErrorMessage} />
-    </Form>
-    : <h2>Invalid token!</h2>}
+                  <Label>Confirm Password</Label>
+                  <Form.Control
+                    className="input textField"
+                    type="password"
+                    isInvalid={passwordError}
+                    placeholder=""
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={setConfirmPassword}
+                  />
+                  <Button
+                    className="input submit-button"
+                    value="Submit"
+                    type="submit"
+                    disabled={submissionErrorMessage}
+                  >
+                    Reset Password
+                  </Button>
+                </Form>
+              </ResponsiveContainer>
+            </FormContainer>
+          </MainSection>
+        </MainContainer>
+      ) : (
+        <h2>Invalid token!</h2>
+      )}
     </>
   );
 };
 
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 0 10px;
+const MainContainer = styled.div`
+  background: linear-gradient(
+      0deg,
+      rgba(33, 37, 41, 0.3),
+      rgba(33, 37, 41, 0.3)
+    ),
+    url(${background});
+  background-size: cover;
+  height: 100vh;
+  font-family: "Roboto", sans-serif;
+  line-height: 1.5;
+  color: #212529;
+
+  @media (max-width: 1007px) {
+    background-position: right;
+  }
+`;
+
+const MainSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const FormContainer = styled.div`
+  padding: 30px;
+  border-radius: 0.25rem;
+  background-color: #f6f7fc;
+
+  @media (max-width: 1007px) {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    border-radius: 25px 25px 0 0;
+    padding: 50px 0;
+  }
+`;
+
+const ResponsiveContainer = styled.div`
+  @media (max-width: 1007px) {
+    padding: 20px;
+  }
+`;
+
+const Header = styled.h3`
+  font-size: 1.75rem;
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  line-height: 1.2;
+`;
+
+const Subheader = styled.p`
+  margin-bottom: 1.5rem;
+  color: #b3b3b3;
 `;
 
 const Label = styled.label`
   margin-bottom: 0.5rem;
 `;
+
 export default ForgotPasswordResetForm;
