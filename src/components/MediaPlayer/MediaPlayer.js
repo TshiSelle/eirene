2f41269c1655e6be188bf6086348e5cf8f0fc938
mt194 <moetraboulsi194@gmail.com>
2022-04-18 11:29:24 +0300

    streaming music from cloudnary

--- src/components/MediaPlayer/MediaPlayer.js
+++ src/components/MediaPlayer/MediaPlayer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactJkMusicPlayer from 'react-jinke-music-player'
 import 'react-jinke-music-player/assets/index.css'
-import Music from './music.mp3';
 import Image from '../HomePage/images/banner.jpg';
 
 const MediaPlayer = ({children}) => {
+    const Music = "https://res.cloudinary.com/cloudloom/video/upload/v1650269930/samples/music/2_Minutes___Lofi_hip_hop_mix_study_sleep_homework_music_vtisxm.ogg";
     return (
         <div>
             <ReactJkMusicPlayer showMediaSession showDownload={false} showThemeSwitch={false}
-                autoHiddenCover={true} remove={false} audioLists={[{ name: 'tiddies', musicSrc: Music }]} />
+                autoHiddenCover={true} remove={false} audioLists={[{ name: 'Lofi cloudinary', musicSrc: Music }]} />
         {children}
         </div>
     )
