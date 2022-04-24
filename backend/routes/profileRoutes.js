83e82f5bfca123d3b0195762fc33f29a39f85471
mt194 <moetraboulsi194@gmail.com>
2022-04-24 20:42:54 +0300

    Api calls for image upload

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { changeName, setProfilePicture } = require('../controllers/profileController');
+const { changeName, setProfilePicture, fetchPic: fetchPic } = require('../controllers/profileController');
 const router = express.Router();
 const fileUpload = require('express-fileupload');
 const verifyJWT = require('../middleware/TokenVerification');
 
 router.patch('/editName', changeName);
+router.get('/getProfilePic', verifyJWT, fetchPic);
 router.post('/setProfilePic', verifyJWT, fileUpload(),  setProfilePicture);
 
 module.exports = router;
