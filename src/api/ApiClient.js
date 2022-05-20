bfcf7e9786a9453bfb16c0561581a33cda3a92d0
mt194 <moetraboulsi194@gmail.com>
2022-05-20 10:57:08 +0300

    Quick global var fix

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import globalVars from '../clientEnvVars'
 
-const host = process.env.NODE_ENV == 'development' ? 'http://localhost:8080' : 'https://eireneserver.herokuapp.com/';
+const host = globalVars.NODE_ENV == 'development' ? 'http://localhost:8080' : 'https://eireneserver.herokuapp.com';
 
 function PostAxiosCall(endpoint, data, headers) {
 	if (!data) data = {};
---
+++ src/clientEnvVars.js
@@ -0,0 +1,6 @@
+const globalVars = {
+	NODE_ENV:  'production'
+};
+
+
+export default globalVars;
