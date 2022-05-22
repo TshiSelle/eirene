742535403d978204b3cb0e9dd61380fbbbcc93bb
OmarHoussami <omar.houssami20@gmail.com>
2022-05-22 16:39:14 +0300

    modified footer

--- src/components/HomePage/Footer.js
+++ src/components/HomePage/Footer.js
@@ -14,12 +14,12 @@ const SiteFooter = () => {
         <p class="about">
           <span> About Eirene</span> The next step to self-care therapy. We at Eirene are driven to help people with their daily life, support their emotional state and always be there for them in their time of need.
         </p>
-        <div class="icons">
+        {/*  <div class="icons">
           <a href="#"><i class="fa fa-facebook"></i></a>
           <a href="#"><i class="fa fa-twitter"></i></a>
           <a href="#"><i class="fa fa-linkedin"></i></a>
           <a href="#"><i class="fa fa-instagram"></i></a>
-        </div>
+  </div> */}
       </div>
       <div class="footer-center col-md-4 col-sm-6">
         <div>
@@ -36,7 +36,7 @@ const SiteFooter = () => {
         </div>
       </div>
       <div class="footer-right col-md-4 col-sm-6">
-        <h2> Eirene<span> <img src="./images/eirene_logo.svg" /></span></h2>
+        <h2> Eirene<div className="eireneImg"> </div></h2>
         <p class="menu">
           <Link to={'/'}>Home</Link> |
           <Link to={'/find-therapists'}> Therapists</Link> |
