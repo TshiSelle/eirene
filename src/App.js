79364ee8d4d9af45088a0e73a17633b5d7d572ff
OmarHoussami <ola088@student.bau.edu.lb>
2022-05-25 18:18:34 +0300

    removed meditation component

--- src/App.js
+++ src/App.js
@@ -7,26 +7,23 @@ import ProfilePage from "./components/Profile/UserProfile/ProfilePage";
 import ForgotPasswordForm from "./components/Profile/SignIn/forgot/ForgotPasswordForm";
 import FullPageSpinner from "./components/spinner/FullPageSpinner";
 import Journal from "./components/Journal/Journal";
-import ForgotPasswordResetForm from './components/Profile/SignIn/forgot/ForgotPasswordResetForm';
+import ForgotPasswordResetForm from "./components/Profile/SignIn/forgot/ForgotPasswordResetForm";
 import SearchTherapists from "./components/Therapists/SearchTherapists";
-import TherapistDescription from './components/Therapists/TherapistDescription';
+import TherapistDescription from "./components/Therapists/TherapistDescription";
 import { AuthProvider } from "./context/AuthContext";
 import NavBar from "./components/HomePage/NavBar";
 import SiteFooter from "./components/HomePage/Footer";
 import { JournalProvider } from "./context/JournalContext";
-import MeditationPage from "./components/Meditations/MeditationPage";
 import VerifyAccount from "./components/Profile/SignUp/verify/VerifyAccount";
 import { UserProvider } from "./context/UserContext";
-import { CalendarProvider } from './context/CalendarContext';
+import { CalendarProvider } from "./context/CalendarContext";
 import MediaPlayer from "./components/MediaPlayer/MediaPlayer";
 import { CloudinaryContext } from "cloudinary-react";
 import ContactUsRoute from "./components/ContactUs/ContactUsRoute";
 import NotFoundRoute from "./components/NotFound/NotFoundRoute";
 
-
 const App = () => {
   return (
-
     <CloudinaryContext cloudName="cloudloom">
       <AuthProvider>
         <UserProvider>
@@ -45,12 +42,26 @@ const App = () => {
                         <Route path="/Profile" element={<ProfilePage />} />
                         <Route path="/SignUp" element={<SignUpPage />} />
                         <Route path="/SignIn" element={<LoginPage />} />
-                        <Route path="/forgot-password" element={<ForgotPasswordForm />} />
-                        <Route path="/forgot-password/:username/:token" element={<ForgotPasswordResetForm />} />
-                        <Route path="/find-therapists" element={<SearchTherapists />} />
-                        <Route path="/Meditations" element={<MeditationPage />} />
-                        <Route path="/therapist-description/:id" element={<TherapistDescription />} />
-                        <Route path="/verify/:username/:token" element={<VerifyAccount />} />
+                        <Route
+                          path="/forgot-password"
+                          element={<ForgotPasswordForm />}
+                        />
+                        <Route
+                          path="/forgot-password/:username/:token"
+                          element={<ForgotPasswordResetForm />}
+                        />
+                        <Route
+                          path="/find-therapists"
+                          element={<SearchTherapists />}
+                        />
+                        <Route
+                          path="/therapist-description/:id"
+                          element={<TherapistDescription />}
+                        />
+                        <Route
+                          path="/verify/:username/:token"
+                          element={<VerifyAccount />}
+                        />
                         <Route path="/Journal" element={<Journal />} />
                         <Route path="/contact" element={<ContactUsRoute />} />
                         <Route path="*" element={<NotFoundRoute />} />
@@ -64,8 +75,7 @@ const App = () => {
           </CalendarProvider>
         </UserProvider>
       </AuthProvider>
-    </CloudinaryContext >
-
+    </CloudinaryContext>
   );
 };
 
