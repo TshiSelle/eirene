919a4a0163e0ed9117a61b1f9073d9ae3111cf5a
OmarHoussami <omar.houssami20@gmail.com>
2022-05-28 17:03:47 +0300

    created account deactivation template

--- backend/helperFunctions/emailSender.js
+++ backend/helperFunctions/emailSender.js
@@ -59,7 +59,7 @@ function sendEmailResetPass(email, username, passResetToken) {
     from: sender,
     to: email,
     subject: "Eirene account reset password",
-    html: `<h3 style="font-size: 24px; margin: 15px 15px 15px 15px;">Hello ${username},</h3>
+    html: `<h3 style="font-size: 24px; margin: 15px 15px 15px 15px;">Hey ${username},</h3>
 	 
     <p style="margin: 5px 15px 15px 15px; font-size: 16px;">
       
@@ -67,7 +67,7 @@ Trouble signing in?<br />
 Resetting your password is easy.<br />
 Just press the button below and follow the instructions. We’ll have you up and running in no time.</p>
 
-<a href=${CLIENT_URL}/verify/${username}/${emailVerificationToken}  style="margin: 15px 15px 15px 15px;"> 
+<a href=${CLIENT_URL}/verify/${username}/${passResetToken}>${CLIENT_URL}/forgot-password/${username}/${passResetToken}  style="margin: 15px 15px 15px 15px;"> 
 <button
 style="margin: 5px 15px 5px 15px;width: 200px;background-color: #eebec3;border: 0;border-radius: 0.5rem;box-sizing: border-box;color: #ffffff;font-family: 'Inter var', ui-sans-serif, system-ui, -apple-system,  system-ui, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans',  sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',  'Noto Color Emoji';font-size: 0.875rem;font-weight: 600;line-height: 1.25rem;padding: 0.75rem 1rem;text-align: center;text-decoration: none #d1d5db solid;text-decoration-thickness: auto;box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),  0 1px 2px 0 rgba(0, 0, 0, 0.06);cursor: pointer;user-select: none;-webkit-user-select: none;touch-action: manipulation;"
   type="button"
@@ -76,9 +76,7 @@ style="margin: 5px 15px 5px 15px;width: 200px;background-color: #eebec3;border: 
     <p style="margin: 5px 15px 15px 15px; font-size: 16px;">
     If you did not make this request then please ignore this email.
     </p>
-    <a href=${CLIENT_URL}/verify/${username}/${passResetToken}>${CLIENT_URL}/forgot-password/${username}/${passResetToken}</a> <br />
     
-      <p style="margin-top: 20px; font-size: 16px;">Welcome to Eirene !</p>
       <p style="margin: 5px 15px 15px 15px; font-size: 16px;">The Eirene Team</p>`
     // html: `Hello! press <a href=${CLIENT_URL}/forgot-password/${username}/${passResetToken}>here</a> to reset your password.`,
   };
@@ -119,7 +117,17 @@ function sendDeactivationEmail(dbUser) {
     from: sender,
     to: email,
     subject: `Eirene account deactivation for ${username}`,
-    html: `Hello! <br />Your account will be deactivated at ${deactivationDate.toLocaleString()} <br />You can stop the deactivation process from inside your account settings.`,
+    html: `<h3 style="font-size: 24px; margin: 15px 15px 15px 15px;">Hello ${username},</h3>
+	 
+    <p style="margin: 5px 15px 15px 15px; font-size: 16px;">
+      
+We're sorry that you're leaving us.<br />
+Your account will be fully deactivated at ${deactivationDate.toLocaleString()}<br />
+If you change your mind before then, feel free to cancel your account deactivation <br/>
+in your profile.</p> <br/>
+<p style="margin-top: 20px; font-size: 16px;">We loved having you with us,</p>
+<p style="margin: 5px 15px 15px 15px; font-size: 16px;">The Eirene Team</p>`
+    //html: `Hello! <br />Your account will be deactivated at ${deactivationDate.toLocaleString()} <br />You can stop the deactivation process from inside your account settings.`,
   };
 
   Transport.sendMail(mailOptions, (error, response) => {
