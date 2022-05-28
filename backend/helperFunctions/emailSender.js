758c045e778333e65f640ef72bee7e4cb6b2285f
OmarHoussami <omar.houssami20@gmail.com>
2022-05-28 16:43:01 +0300

    forgot password email template

--- backend/helperFunctions/emailSender.js
+++ backend/helperFunctions/emailSender.js
@@ -59,7 +59,28 @@ function sendEmailResetPass(email, username, passResetToken) {
     from: sender,
     to: email,
     subject: "Eirene account reset password",
-    html: `Hello! press <a href=${CLIENT_URL}/forgot-password/${username}/${passResetToken}>here</a> to reset your password.`,
+    html: `<h3 style="font-size: 24px; margin: 15px 15px 15px 15px;">Hello ${username},</h3>
+	 
+    <p style="margin: 5px 15px 15px 15px; font-size: 16px;">
+      
+Trouble signing in?<br />
+Resetting your password is easy.<br />
+Just press the button below and follow the instructions. We’ll have you up and running in no time.</p>
+
+<a href=${CLIENT_URL}/verify/${username}/${emailVerificationToken}  style="margin: 15px 15px 15px 15px;"> 
+<button
+style="margin: 5px 15px 5px 15px;width: 200px;background-color: #eebec3;border: 0;border-radius: 0.5rem;box-sizing: border-box;color: #ffffff;font-family: 'Inter var', ui-sans-serif, system-ui, -apple-system,  system-ui, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans',  sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',  'Noto Color Emoji';font-size: 0.875rem;font-weight: 600;line-height: 1.25rem;padding: 0.75rem 1rem;text-align: center;text-decoration: none #d1d5db solid;text-decoration-thickness: auto;box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),  0 1px 2px 0 rgba(0, 0, 0, 0.06);cursor: pointer;user-select: none;-webkit-user-select: none;touch-action: manipulation;"
+  type="button"
+>Reset Password</button></a>
+  
+    <p style="margin: 5px 15px 15px 15px; font-size: 16px;">
+    If you did not make this request then please ignore this email.
+    </p>
+    <a href=${CLIENT_URL}/verify/${username}/${passResetToken}>${CLIENT_URL}/forgot-password/${username}/${passResetToken}</a> <br />
+    
+      <p style="margin-top: 20px; font-size: 16px;">Welcome to Eirene !</p>
+      <p style="margin: 5px 15px 15px 15px; font-size: 16px;">The Eirene Team</p>`
+    // html: `Hello! press <a href=${CLIENT_URL}/forgot-password/${username}/${passResetToken}>here</a> to reset your password.`,
   };
 
   Transport.sendMail(mailOptions, (error, response) => {
