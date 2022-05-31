8dc8f61f3f2a129a73f8ee00880d753d2e27bc1f
lynn <lynn.serhan2001@gmail.com>
2022-05-31 15:34:17 +0300

    deactivation module design

--- src/components/Profile/UserProfile/DeactivationModal.js
+++ src/components/Profile/UserProfile/DeactivationModal.js
@@ -5,8 +5,13 @@ import { DeactivateAccount, ReactivateAccount } from "../../../api/ApiClient";
 import LoadingSpinner from "../../LoadingSpinner/LoadingSpinner";
 import { useUser } from "../../../context/UserContext";
 
-
-const DeactivationModal = ({ showModal, closeModal, authToken, setDate, setMessage }) => {
+const DeactivationModal = ({
+  showModal,
+  closeModal,
+  authToken,
+  setDate,
+  setMessage,
+}) => {
   const [errorMessage, setErrorMessage] = useState(null);
   const { user } = useUser();
   const [isLoading, setLoading] = useState(false);
@@ -16,8 +21,8 @@ const DeactivationModal = ({ showModal, closeModal, authToken, setDate, setMessa
       .then((response) => {
         if (response.data.success) {
           setLoading(false);
-		  setDate(true)
-		  setMessage("Account entered deactivation process");
+          setDate(true);
+          setMessage("Account entered deactivation process");
           closeModal();
         } else {
           setLoading(false);
@@ -30,18 +35,31 @@ const DeactivationModal = ({ showModal, closeModal, authToken, setDate, setMessa
       });
   };
   return (
-    <Modal onHide={closeModal} show={showModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal
+      onHide={closeModal}
+      show={showModal}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Are you sure you want to proceed with deactivating your account?</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Are you sure you want to proceed with deactivating your account?
+        </Modal.Title>
       </Modal.Header>
       <div style={{ padding: "16px" }}>
-        <p>Deactivation process will take 10 days, You will be able to reactivate your account whenever you want.</p>
+        <p>
+          Deactivation process will take 10 days, You will be able to reactivate
+          your account whenever you want.
+        </p>
         {<h2 style={{ color: "red" }}>{errorMessage}</h2>}
       </div>
       <Modal.Footer>
-        <Button onClick={handleDeactivation}>Deactivate Account</Button>
+        <Button onClick={handleDeactivation} className="confirm-deactivate">
+          Deactivate Account
+        </Button>
       </Modal.Footer>
-	  <LoadingSpinner display={isLoading} />
+      <LoadingSpinner display={isLoading} />
     </Modal>
   );
 };
