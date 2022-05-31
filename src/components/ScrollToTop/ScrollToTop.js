f54b0abcc04ccc43fb5c095abadefe54a93e04b2
lynn <lynn.serhan2001@gmail.com>
2022-05-31 10:25:02 +0300

    close navbar when changing page

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -5,9 +5,12 @@ const ScrollToTop = (props) => {
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
+    {
+      document.getElementById("menu_items").style.width = "0";
+    }
   }, [location]);
 
-  return <>{props.children}</>
+  return <>{props.children}</>;
 };
 
 export default ScrollToTop;
